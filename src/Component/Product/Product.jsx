2498666b049505cb3cc6e64560fd0a8bf1d7f797
Ridoy-Mojumder <hridoymoju463@gmail.com>
2024-06-28 02:53:16 +0600

    make the view details page

--- src/Component/Product/Product.jsx
+++ src/Component/Product/Product.jsx
@@ -1,9 +1,14 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { Spin, Layout, Row, Col, Card, Button } from "antd";
+import { LoadingOutlined } from "@ant-design/icons";
+
+const { Content } = Layout;
 
 const Product = () => {
     const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(true); // State to manage loading state
     const [showAll, setShowAll] = useState(false); // State to manage show more functionality
 
     useEffect(() => {
@@ -11,8 +16,10 @@ const Product = () => {
             try {
                 const response = await axios.get('https://dummyjson.com/products');
                 setProducts(response.data.products);
+                setLoading(false); // Set loading to false after fetching data
             } catch (error) {
                 console.error("Error fetching products:", error);
+                setLoading(false); // Set loading to false in case of error
             }
         };
 
@@ -31,35 +38,51 @@ const Product = () => {
     };
 
     return (
-        <div className="container mx-auto p-4">
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3  gap-6">
-                {products.slice(0, showAll ? products.length : 6).map(product => (
-                    <div key={product.id} className="bg-white p-4 rounded-lg shadow-md">
-                        <img src={product.thumbnail} alt={product.title} className="w-full h-48 object-cover rounded-t-lg" />
-                        <div className="p-4">
-                            <h2 className="text-xl font-semibold">{product.title}</h2>
-                            <p className="text-gray-600 mt-2">{truncateDescription(product.description, 100)}</p>
-                            <p className="text-gray-800 mt-2">Price: ${product.price}</p>
-                            <p className="text-gray-800 mt-2">Rating: {product.rating}</p>
-                            <p className="text-gray-800 mt-2">Stock: {product.stock}</p>
-                            <Link to={`/product/${product.id}`} className="mt-4 inline-block bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
-                                View Details
-                            </Link>
+        <div className="min-h-screen">
+            <Content className="p-4 ">
+                {loading ? (
+                    <div className="flex items-center justify-center h-screen text-white">
+                        <div className="text-center">
+                            <div className="mb-4">
+                                <Spin indicator={<LoadingOutlined style={{ fontSize: 48, color: "#1890ff" }} spin />} />
+                            </div>
+                            <p className="text-lg font-semibold ">Loading products...</p>
                         </div>
                     </div>
-                ))}
-            </div>
-            {/* Show more button */}
-            {!showAll && (
-                <div className="mt-8 text-center">
-                    <button
-                        onClick={toggleShowAll}
-                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded shadow-lg transform transition-transform duration-300 hover:scale-105"
-                    >
-                        Show More
-                    </button>
-                </div>
-            )}
+                ) : (
+                    <Row gutter={[16, 16]}>
+                        {products.slice(0, showAll ? products.length : 6).map(product => (
+                            <Col key={product.id} xs={24} sm={12} md={8}>
+                                <Card className="rounded-lg shadow-md border-none ">
+                                    <img src={product.thumbnail} alt={product.title} className="w-full h-48 object-cover rounded-t-lg" />
+                                    <div className="p-4">
+                                        <h2 className="text-xl font-semibold">{product.title}</h2>
+                                        <p className="text-gray-600 mt-2">{truncateDescription(product.description, 100)}</p>
+                                        <p className="text-gray-800 mt-2">Price: ${product.price}</p>
+                                        <p className="text-gray-800 mt-2">Rating: {product.rating}</p>
+                                        <p className="text-gray-800 mt-2">Stock: {product.stock}</p>
+                                        <Link to={`/product/${product.id}`} className="mt-4 inline-block bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
+                                            View Details
+                                        </Link>
+                                    </div>
+                                </Card>
+                            </Col>
+                        ))}
+                    </Row>
+                )}
+                {/* Show more button */}
+                {!showAll && (
+                    <div className="mt-8 text-center">
+                        <Button
+                            onClick={toggleShowAll}
+                            type="primary"
+                            className="transform transition-transform duration-300 hover:scale-105"
+                        >
+                            Show More
+                        </Button>
+                    </div>
+                )}
+            </Content>
         </div>
     );
 };
--- src/Component/Product/ProductDetails.jsx
+++ src/Component/Product/ProductDetails.jsx
@@ -1,10 +1,13 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import { Spin, Layout, Row, Col, Form, Input, Card, Tag, Divider } from "antd";
 import axios from "axios";
-import { Spin } from "antd";
+import { LoadingOutlined } from "@ant-design/icons";
+
+const { Content } = Layout;
 
 const ProductDetails = () => {
-    const { id } = useParams(); // Get the product ID from route params
+    const { id } = useParams();
     const [product, setProduct] = useState(null);
     const [loading, setLoading] = useState(true);
 
@@ -24,74 +27,139 @@ const ProductDetails = () => {
     }, [id]);
 
     if (loading) {
-        return <Spin size="large" className="spinner" />;
+        return (
+            <div className="flex items-center justify-center h-screen">
+                <div className="text-center">
+                    <div className="mb-4">
+                        <Spin indicator={<LoadingOutlined style={{ fontSize: 48, color: "#1890ff" }} spin />} />
+                    </div>
+                    <p className="text-lg font-semibold text-gray-800">Loading products...</p>
+                </div>
+            </div>
+        );
     }
 
     return (
-        <div className="container mx-auto p-4">
-            {product && (
-                <div className="max-w-4xl mx-auto bg-white shadow-md rounded-lg overflow-hidden">
-                    <div className="">
-                        <div className="md:flex-shrink-0">
-                            <img src={product.thumbnail} alt={product.title} className="h-64 w-full object-cover md:w-[600px]" />
-                        </div>
-                        <div className="p-8">
-                            <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">{product.category}</div>
-                            <h1 className="block mt-1 text-3xl font-semibold text-gray-900">{product.title}</h1>
-                            <p className="mt-2 text-gray-600">{product.description}</p>
-                            <div className="mt-4">
-                                <h2 className="text-xl font-semibold">Details</h2>
-                                <ul className="list-disc list-inside mt-2">
-                                    <li><strong>Price:</strong> ${product.price}</li>
-                                    <li><strong>Discount:</strong> {product.discountPercentage}%</li>
-                                    <li><strong>Rating:</strong> {product.rating}</li>
-                                    <li><strong>Stock:</strong> {product.stock}</li>
-                                    <li><strong>Brand:</strong> {product.brand}</li>
-                                    <li><strong>SKU:</strong> {product.sku}</li>
-                                    <li><strong>Weight:</strong> {product.weight} g</li>
-                                    <li><strong>Dimensions:</strong> {product.dimensions.width}cm x {product.dimensions.height}cm x {product.dimensions.depth}cm</li>
-                                    <li><strong>Warranty:</strong> {product.warrantyInformation}</li>
-                                    <li><strong>Shipping:</strong> {product.shippingInformation}</li>
-                                    <li><strong>Availability:</strong> {product.availabilityStatus}</li>
-                                </ul>
-                            </div>
+        <Layout className="min-h-screen">
+            <Content className="p-4">
+                {product && (
+                    <Card className="mx-auto max-w-6xl">
+                        <Row gutter={[16, 16]}>
+                            <Col span={24} className="flex justify-center items-center">
+                                <img src={product.thumbnail} alt={product.title} className="max-w-full h-auto mb-4" />
+                            </Col>
 
-                            <div className="mt-4">
-                                <h2 className="text-xl font-semibold">Tags</h2>
-                                <div className="flex flex-wrap mt-2">
-                                    {product.tags.map((tag, index) => (
-                                        <span key={index} className="bg-gray-200 px-2 py-1 mr-2 mb-2 rounded">{tag}</span>
-                                    ))}
+                            <Col span={24}>
+                                <div className="p-4">
+                                    <h1 className="text-3xl font-semibold">{product.title}</h1>
+                                    <p className="text-gray-600">{product.description}</p>
+
+                                    <Divider />
+
+                                    <Form layout="vertical">
+                                        <Row gutter={[16, 16]}>
+                                            <Col xs={24} sm={12}>
+                                                <Form.Item label="Price">
+                                                    <Input prefix="$" value={product.price.toFixed(2)} readOnly />
+                                                </Form.Item>
+                                            </Col>
+                                            <Col xs={24} sm={12}>
+                                                <Form.Item label="Rating">
+                                                    <Input value={product.rating} readOnly />
+                                                </Form.Item>
+                                            </Col>
+                                            <Col xs={24} sm={12}>
+                                                <Form.Item label="Stock">
+                                                    <Input value={product.stock} readOnly />
+                                                </Form.Item>
+                                            </Col>
+                                            <Col xs={24} sm={12}>
+                                                <Form.Item label="Brand">
+                                                    <Input value={product.brand} readOnly />
+                                                </Form.Item>
+                                            </Col>
+                                            <Col xs={24} sm={12}>
+                                                <Form.Item label="SKU">
+                                                    <Input value={product.sku} readOnly />
+                                                </Form.Item>
+                                            </Col>
+                                            <Col xs={24} sm={12}>
+                                                <Form.Item label="Weight">
+                                                    <Input value={`${product.weight} g`} readOnly />
+                                                </Form.Item>
+                                            </Col>
+                                            <Col xs={24} sm={12}>
+                                                <Form.Item label="Dimensions">
+                                                    <Input
+                                                        value={`${product.dimensions.width}cm x ${product.dimensions.height}cm x ${product.dimensions.depth}cm`}
+                                                        readOnly
+                                                    />
+                                                </Form.Item>
+                                            </Col>
+                                            <Col xs={24} sm={12}>
+                                                <Form.Item label="Warranty">
+                                                    <Input value={product.warrantyInformation} readOnly />
+                                                </Form.Item>
+                                            </Col>
+                                            <Col xs={24} sm={12}>
+                                                <Form.Item label="Shipping">
+                                                    <Input value={product.shippingInformation} readOnly />
+                                                </Form.Item>
+                                            </Col>
+                                            <Col xs={24} sm={12}>
+                                                <Form.Item label="Availability">
+                                                    <Input value={product.availabilityStatus} readOnly />
+                                                </Form.Item>
+                                            </Col>
+                                            <Col xs={24}>
+                                                <Form.Item label="Tags">
+                                                    <div className="flex flex-wrap">
+                                                        {product.tags.map((tag, index) => (
+                                                            <Tag key={index}>{tag}</Tag>
+                                                        ))}
+                                                    </div>
+                                                </Form.Item>
+                                            </Col>
+                                            <Col xs={24}>
+                                                <Form.Item label="Reviews">
+                                                    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+                                                        {product.reviews.map((review, index) => (
+                                                            <Card key={index} className="mb-2 p-4">
+                                                                <p><strong>Rating:</strong> {review.rating}</p>
+                                                                <p><strong>Reviewer:</strong> {review.reviewerName}</p>
+                                                                <p><strong>Email:</strong> {review.reviewerEmail}</p>
+                                                                <p><strong>Comment:</strong> {review.comment}</p>
+                                                                <p><strong>Date:</strong> {new Date(review.date).toLocaleDateString()}</p>
+                                                            </Card>
+                                                        ))}
+                                                    </div>
+                                                </Form.Item>
+                                            </Col>
+
+                                            <Col xs={24}>
+                                                <Form.Item label="Meta Information">
+                                                    <ul className="list-disc list-inside mt-2">
+                                                        <li><strong>Created At:</strong> {new Date(product.meta.createdAt).toLocaleDateString()}</li>
+                                                        <li><strong>Updated At:</strong> {new Date(product.meta.updatedAt).toLocaleDateString()}</li>
+                                                        <li><strong>Barcode:</strong> {product.meta.barcode}</li>
+                                                        <li><strong>QR Code:</strong> <img src={product.meta.qrCode} alt="QR Code" className="mt-2" style={{ maxWidth: "100%", maxHeight: "200px" }} /></li>
+                                                    </ul>
+                                                </Form.Item>
+                                            </Col>
+                                        </Row>
+                                    </Form>
                                 </div>
-                            </div>
-
-                            <div className="mt-4">
-                                <h2 className="text-xl font-semibold">Reviews</h2>
-                                {product.reviews.map((review, index) => (
-                                    <div key={index} className="bg-gray-100 p-4 rounded-lg mt-2">
-                                        <p><strong>Rating:</strong> {review.rating}</p>
-                                        <p><strong>Reviewer:</strong> {review.reviewerName}</p>
-                                        <p><strong>Email:</strong> {review.reviewerEmail}</p>
-                                        <p><strong>Comment:</strong> {review.comment}</p>
-                                        <p><strong>Date:</strong> {new Date(review.date).toLocaleDateString()}</p>
-                                    </div>
-                                ))}
-                            </div>
-
-                            <div className="mt-4">
-                                <h2 className="text-xl font-semibold">Meta Information</h2>
-                                <ul className="list-disc list-inside mt-2">
-                                    <li><strong>Created At:</strong> {new Date(product.meta.createdAt).toLocaleDateString()}</li>
-                                    <li><strong>Updated At:</strong> {new Date(product.meta.updatedAt).toLocaleDateString()}</li>
-                                    <li><strong>Barcode:</strong> {product.meta.barcode}</li>
-                                    <li><strong>QR Code:</strong> <img src={product.meta.qrCode} alt="QR Code" className="mt-2" style={{ maxWidth: "100%", maxHeight: "200px" }} /></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
+                            </Col>
+                        </Row>
+                    </Card>
+                )}
+                <div className="mt-4 flex justify-center">
+                    <Link to="/" className="bg-blue-500 hover:bg-blue-600  text-white px-4 py-2 rounded">
+                        Back Home
+                    </Link>
                 </div>
-            )}
-        </div>
+            </Content>
+        </Layout>
     );
 };
 
